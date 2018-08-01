03fde3fd777a50f813b254af37bebd4b466d539a
Ryan Piper <piper.ryan235@gmail.com>
2018-08-01 07:25:00 -0600

    feat: add the sql fmt module

--- src/interval_fmt/mod.rs
+++ src/interval_fmt/mod.rs
@@ -1,2 +1,17 @@
 pub mod iso_8601;
 pub mod postgre_style;
+pub mod sql;
+
+fn safe_abs_u32(mut num: i32) -> u32 {
+    let max = i32::max_value(); 
+    let max_min = max * -1; 
+    if num <= max_min {
+        let result = max as u32; 
+        num += max; 
+        num = num * -1; 
+        let result = result + num as u32;
+        result 
+    } else {
+        num.abs() as u32
+    }
+}
