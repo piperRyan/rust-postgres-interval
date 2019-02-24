af4741c56c48ff55d28e3a3d1888846cff51bb19
Ryan Piper <piper.ryan235@gmail.com>
2019-02-24 10:45:49 -0600

    refactor(*): split interval functionality into smaller modules and redo formatting logic

---
+++ src/pg_formatter.rs
@@ -0,0 +1,219 @@
+use pg_interval::Interval;
+
+pub struct IntervalNorm {
+    pub years: i32,
+    pub months: i32,
+    pub days: i32,
+    pub hours: i64,
+    pub minutes: i64,
+    pub seconds: i64,
+    pub microseconds: i64,
+}
+
+impl<'a> From<&'a Interval> for IntervalNorm {
+    fn from(val: &Interval) -> IntervalNorm {
+        // grab the base values from the interval
+        let months = val.months;
+        let days = val.days;
+        let microseconds = val.microseconds;
+        // calc the year and get the remaining months
+        let years = (months - (months % 12)) / 12;
+        let months = months - years * 12;
+        // calc the hours from the microseconds and update
+        // the remaining microseconds.
+        let hours = (microseconds - (microseconds % 3600000000)) / 3600000000;
+        let microseconds = microseconds - hours * 3600000000;
+        // calc the minutes from remaining microseconds and
+        // update the remaining microseconds.
+        let minutes = (microseconds - (microseconds % 60000000)) / 60000000;
+        let microseconds = microseconds - minutes * 60000000;
+        // calc the seconds and update the remaining microseconds.
+        let seconds = (microseconds - (microseconds % 1000000)) / 1000000;
+        let microseconds = microseconds - seconds * 1000000;
+        IntervalNorm {
+            years,
+            months,
+            days,
+            hours,
+            minutes,
+            seconds,
+            microseconds,
+        }
+    }
+}
+
+impl IntervalNorm {
+    /// Is all the values in the interval set to 0?
+    fn is_zeroed(&self) -> bool {
+        return self.years == 0
+            && self.months == 0
+            && self.hours == 0
+            && self.minutes == 0
+            && self.seconds == 0
+            && self.microseconds == 0;
+    }
+
+    /// Is the years or month value set?
+    fn is_year_month_present(&self) -> bool {
+        return self.years != 0 || self.months != 0;
+    }
+
+    /// Is the day value set?
+    fn is_day_present(&self) -> bool {
+        return self.days != 0;
+    }
+
+    /// Is at least one of hours,minutes,seconds,microseconds values
+    /// postive. There are no mixed intervals so we can assume that
+    /// if one value is postive the rest are at least >= 0
+    fn is_time_interval_pos(&self) -> bool {
+        self.hours > 0 || self.minutes > 0 || self.seconds > 0 || self.microseconds > 0
+    }
+
+    /// Is the hours,minutes, seconds, microseconds values set?
+    fn is_time_present(&self) -> bool {
+        self.hours != 0 || self.minutes != 0 || self.seconds != 0 || self.microseconds != 0
+    }
+
+    /// The sql and postgres standard for the time intervals format
+    /// are very similar. This gets the base string that is
+    /// shared between them.
+    fn get_sql_postgres_time_interval(&self) -> String {
+        let mut time_interval = "".to_owned();
+        if self.is_time_present() {
+            let mut sign = "".to_owned();
+            if !self.is_time_interval_pos() {
+                sign = "-".to_owned();
+            }
+            time_interval.push_str(
+                &*(sign
+                    + &pad_i64(self.hours)
+                    + ":"
+                    + &pad_i64(self.minutes)
+                    + ":"
+                    + &pad_i64(self.seconds)),
+            );
+            if self.microseconds != 0 {
+                time_interval.push_str(&*format!(".{:06}", self.microseconds))
+            }
+        }
+        time_interval
+    }
+
+    /// Produces a iso 8601 compliant interval string.
+    pub fn to_iso_8601(self) -> String {
+        if self.is_zeroed() {
+            return "PT0S".to_owned();
+        }
+        let mut year_interval = "P".to_owned();
+        let mut day_interval = "".to_owned();
+        let mut time_interval;
+        if self.is_time_present() {
+            time_interval = "T".to_owned();
+            if self.hours != 0 {
+                time_interval.push_str(&format!("{}H", self.hours));
+            }
+            if self.minutes != 0 {
+                time_interval.push_str(&format!("{}M", self.minutes));
+            }
+            if self.seconds != 0 {
+                time_interval.push_str(&format!("{}S", self.seconds));
+            }
+            if self.microseconds != 0 {
+                let ms = safe_abs_u64(self.microseconds);
+                time_interval.push_str(&format!(".{:06}", ms));
+            }
+        } else {
+            time_interval = "".to_owned();
+        }
+        if self.years != 0 {
+            year_interval.push_str(&format!("{}Y", self.years));
+        }
+        if self.months != 0 {
+            year_interval.push_str(&format!("{}M", self.months));
+        }
+        if self.days != 0 {
+            day_interval.push_str(&format!("{}D", self.days));
+        }
+        year_interval.push_str(&*day_interval);
+        year_interval.push_str(&*time_interval);
+        return year_interval;
+    }
+
+    /// Produces a postgres compliant interval string.
+    pub fn to_postgres(self) -> String {
+        if self.is_zeroed() {
+            return "00:00:00".to_owned();
+        }
+        let mut year_interval = "".to_owned();
+        let mut day_interval = "".to_owned();
+        let time_interval = self.get_sql_postgres_time_interval();
+        if self.is_day_present() {
+            day_interval = format!("{:#?} days ", self.days)
+        }
+        if self.is_year_month_present() {
+            if self.years != 0 {
+                year_interval.push_str(&*format!("{:#?} year ", self.years))
+            }
+            if self.months != 0 {
+                year_interval.push_str(&*format!("{:#?} mons ", self.months));
+            }
+        }
+        year_interval.push_str(&*day_interval);
+        year_interval.push_str(&*time_interval);
+        year_interval.trim().to_owned()
+    }
+
+    /// Produces a compliant sql interval string.
+    pub fn to_sql(self) -> String {
+        if self.is_zeroed() {
+            return "0".to_owned();
+        }
+        let mut year_interval = "".to_owned();
+        let mut day_interval = "".to_owned();
+        let mut time_interval = self.get_sql_postgres_time_interval();
+        if self.is_time_interval_pos() {
+            time_interval = "+".to_owned() + &time_interval
+        }
+        if self.is_day_present() {
+            day_interval = format!("{:#?} ", self.days);
+        }
+        if self.is_year_month_present() {
+            let mut sign = "+".to_owned();
+            if self.years < 0 || self.months < 0 {
+                sign = "-".to_owned();
+            }
+            year_interval = format!("{}{}-{} ", sign, self.years, self.months);
+        }
+        year_interval.push_str(&day_interval);
+        year_interval.push_str(&time_interval);
+        return year_interval.trim().to_owned();
+    }
+}
+
+/// Safely maps a i64 value to a unsigned number
+/// without any overflow issues.
+fn safe_abs_u64(mut num: i64) -> u64 {
+    let max = i64::max_value();
+    let max_min = max * -1;
+    if num <= max_min {
+        let result = max as u64;
+        num += max;
+        num = num * -1;
+        let result = result + num as u64;
+        result
+    } else {
+        num.abs() as u64
+    }
+}
+
+/// Pads a i64 value with a width of 2.
+fn pad_i64(val: i64) -> String {
+    let num;
+    if val < 0 {
+        num = safe_abs_u64(val);
+    } else {
+        num = val as u64;
+    }
+    return format!("{:02}", num);
+}
---
+++ src/pg_interval_add.rs
@@ -0,0 +1,154 @@
+use pg_interval::Interval;
+use std::ops;
+
+impl Interval {
+    /// Checked interval addition. Computes `Interval + Interval` and `None` if there
+    /// was an overflow.
+    pub fn checked_add(self, other_interval: Interval) -> Option<Interval> {
+        Some(Interval {
+            months: self.months.checked_add(other_interval.months)?,
+            days: self.days.checked_add(other_interval.days)?,
+            microseconds: self.microseconds.checked_add(other_interval.microseconds)?,
+        })
+    }
+
+    /// Shortcut method to add day time part to the interval. Any units smaller than a microsecond
+    /// will be truncated.
+    pub fn add_day_time(self, days: i32, hours: i64, minutes: i64, seconds: f64) -> Interval {
+        let hours_as_micro: i64 = hours * 3600000000;
+        let minutes_as_micro: i64 = minutes * 60000000;
+        let seconds_as_micro: i64 = (seconds * 1000000.0).floor() as i64;
+        let additional_micro: i64 = hours_as_micro + minutes_as_micro + seconds_as_micro;
+        Interval {
+            months: self.months,
+            days: self.days + days,
+            microseconds: self.microseconds + additional_micro,
+        }
+    }
+
+    /// Checked day time interval addition. Computes the interval and will return `None` if a
+    /// overflow has occured. Any units smaller than a microsecond will be truncated.
+    pub fn checked_add_day_time(
+        self,
+        days: i32,
+        hours: i64,
+        minutes: i64,
+        seconds: f64,
+    ) -> Option<Interval> {
+        let hours_as_micro: i64 = hours.checked_mul(3600000000)?;
+        let minutes_as_micro: i64 = minutes.checked_mul(60000000)?;
+        let seconds_as_micro: i64 = (seconds * 1000000.0).floor() as i64;
+        let additional_micro: i64 = hours_as_micro
+            .checked_add(minutes_as_micro)?
+            .checked_add(seconds_as_micro)?;
+        Some(Interval {
+            months: self.months,
+            days: self.days.checked_add(days)?,
+            microseconds: self.microseconds.checked_add(additional_micro)?,
+        })
+    }
+
+    /// Adds a year month interval.
+    pub fn add_year_month(self, year: i32, months: i32) -> Interval {
+        let years_as_months = year * 12;
+        Interval {
+            months: self.months + years_as_months + months,
+            days: self.days,
+            microseconds: self.microseconds,
+        }
+    }
+
+    /// Checked year month addition. Computes the interval and will return `None` if a
+    /// overflow has occured.
+    pub fn checked_add_year_month(self, year: i32, months: i32) -> Option<Interval> {
+        let years_as_months = year.checked_mul(12)?;
+        let additional_months = years_as_months.checked_add(months)?;
+        Some(Interval {
+            months: self.months.checked_add(additional_months)?,
+            days: self.days,
+            microseconds: self.microseconds,
+        })
+    }
+}
+
+impl ops::Add for Interval {
+    type Output = Interval;
+    fn add(self, other_interval: Interval) -> Interval {
+        Interval {
+            months: self.months + other_interval.months,
+            days: self.days + other_interval.days,
+            microseconds: self.microseconds + other_interval.microseconds,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use pg_interval::Interval;
+    #[test]
+    fn test_checked_add() {
+        let interval = Interval::new(13, 0, 0);
+        let interval_add = Interval::new(2, 1, 12);
+        let result = interval.checked_add(interval_add);
+        assert_eq!(result, Some(Interval::new(15, 1, 12)));
+    }
+
+    #[test]
+    fn test_checked_add_2() {
+        let interval = Interval::new(13, 0, 0);
+        let interval_add = Interval::new(i32::max_value(), 1, 12);
+        let result = interval.checked_add(interval_add);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_checked_add_day_time() {
+        let interval = Interval::new(13, 0, 0);
+        let result = interval.checked_add_day_time(2, 0, 0, 2.123456789);
+        assert_eq!(result, Some(Interval::new(13, 2, 2123456)));
+    }
+
+    #[test]
+    fn test_checked_add_day_time_2() {
+        let interval = Interval::new(13, i32::max_value(), 0);
+        let result = interval.checked_add_day_time(200, 0, 0, 2.123456789);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_add_year_month() {
+        let interval = Interval::new(13, 0, 0);
+        let result = interval.add_year_month(1, 1);
+        assert_eq!(result, Interval::new(26, 0, 0));
+    }
+
+    #[test]
+    fn checked_add_year_month_1() {
+        let interval = Interval::new(15, 0, 0);
+        let result = interval.checked_add_year_month(1, 1);
+        assert_eq!(result, Some(Interval::new(28, 0, 0)));
+    }
+
+    #[test]
+    fn checked_add_year_month_2() {
+        let interval = Interval::new(15, 0, 0);
+        let result = interval.checked_add_year_month(i32::max_value(), 32);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_add() {
+        let interval = Interval::new(0, 0, 0);
+        let other_interval = Interval::new(10, 0, 0);
+        let result = interval + other_interval;
+        assert_eq!(result, Interval::new(10, 0, 0));
+    }
+
+    #[test]
+    fn test_add_day_time() {
+        let interval = Interval::new(13, 0, 0);
+        let result = interval.add_day_time(2, 0, 0, 2.123456789);
+        assert_eq!(result, Interval::new(13, 2, 2123456));
+    }
+
+}
