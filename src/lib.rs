2756ce518d1996626d9c179d562c46e474650e88
Ryan Piper <piper.ryan235@gmail.com>
2019-02-24 10:41:17 -0600

    build: update the module names to the new ones

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-#[cfg(feature="postgres")]
+#[cfg(feature = "postgres")]
 #[macro_use]
 extern crate postgres;
-#[cfg(feature="postgres")]
+#[cfg(feature = "postgres")]
 extern crate byteorder;
 
-#[cfg(feature="postgres")]
-mod services;
+#[cfg(feature = "postgres")]
+mod integrations;
 
-mod interval_fmt;
-mod pg_interval; 
+mod pg_formatter;
+mod pg_interval;
+mod pg_interval_add;
+mod pg_interval_sub;
 
 pub use pg_interval::Interval;
