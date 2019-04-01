346c26d2e093a0631352c682369549c5a7978c89
Ryan Piper <piper.ryan235@gmail.com>
2019-03-31 22:00:32 -0600

    refactor(*): move common scaling and static values to mod file

--- src/interval_parse/iso_8601.rs
+++ src/interval_parse/iso_8601.rs
@@ -1,20 +1,25 @@
 use pg_interval::Interval;
 use super::parse_error::ParseError;
 use interval_norm::IntervalNorm;
-
-static DAYS_PER_MONTH: i32 = 30;
-static MONTHS_PER_YEAR: i32 = 12;
-static SECONDS_PER_MIN: i32 = 60;
-static HOURS_PER_DAY: i32 = 24;
-static MINUTES_PER_HOUR: i32 = 60;
-static MICROS_PER_SECOND: i32 = 1_000_000;
+use super::{
+    scale_date, 
+    scale_time, 
+    DAYS_PER_MONTH,
+    MONTHS_PER_YEAR,
+    SECONDS_PER_MIN,
+    HOURS_PER_DAY,
+    MINUTES_PER_HOUR,
+    MICROS_PER_SECOND
+};
 
 
 enum ParserCode {
     BADFORMAT, 
     GOOD,
-    DELIMFOUND
+    DELIMFOUND,
 }
+
+
 
 impl Interval {
     pub fn from_iso<'a>(iso_str: &'a str) -> Result<Interval,ParseError> {
@@ -125,26 +130,6 @@ fn parse_number<'a> (number: &'a mut String) -> Result<f64, ParseError> {
         Ok(parse_num)
     }
 }
-
-fn scale_date(val: f64, scale: i32) -> (i32, i32) {
-    if val.fract() == 0.0 {
-        return (val.trunc() as i32, 0)
-    } else {
-        // matches postgres implementation of just truncating.
-        let sub_value = (val.fract() * scale as f64).round() as i32;
-        (val.trunc() as i32, sub_value)
-    }
-}
-
-fn scale_time(val: f64, scale: i32) -> (i64, i64) {
-      if val.fract() == 0.0 {
-        return (val.trunc() as i64, 0)
-    } else {
-        // matches postgres implementation of just truncating.
-        let sub_value = (val.fract() * scale as f64).round() as i64;
-        (val.trunc() as i64, sub_value)
-    }
-} 
 
 
 #[cfg(test)]
