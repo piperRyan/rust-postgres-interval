6d3310dc6e5a81ce510785eea60bc71451de448a
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-12-19 00:13:37 -0500

    Upgrade to new postgres_types API
    The FromSql and ToSql traits have been moved into a new crate,
    postgres-types, which some minor modifications.

--- src/integrations/rust_postgres.rs
+++ src/integrations/rust_postgres.rs
@@ -1,11 +1,12 @@
 use std::error::Error;
 
-use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
-use postgres::types::{FromSql, IsNull, ToSql, Type, INTERVAL};
+use byteorder::{BigEndian, ReadBytesExt};
+use bytes::{BufMut, BytesMut};
+use postgres_types::{FromSql, IsNull, ToSql, Type};
 
 use crate::Interval;
 
-impl FromSql for Interval {
+impl<'a> FromSql<'a> for Interval {
     fn from_sql(_: &Type, mut raw: &[u8]) -> Result<Interval, Box<dyn Error + Sync + Send>> {
         let microseconds: i64 = raw.read_i64::<BigEndian>()?;
         let days: i32 = raw.read_i32::<BigEndian>()?;
@@ -19,28 +20,28 @@ impl FromSql for Interval {
     }
 
     fn accepts(ty: &Type) -> bool {
-        match *ty {
-            INTERVAL => true,
+        match ty {
+            &Type::INTERVAL => true,
             _ => false,
         }
     }
 }
 
 impl ToSql for Interval {
-    fn to_sql(&self, _: &Type, out: &mut Vec<u8>) -> Result<IsNull, Box<dyn Error + Sync + Send>> {
-        out.write_i64::<BigEndian>(self.microseconds)?;
-        out.write_i32::<BigEndian>(self.days)?;
-        out.write_i32::<BigEndian>(self.months)?;
+    fn to_sql(&self, _: &Type, out: &mut BytesMut) -> Result<IsNull, Box<dyn Error + Sync + Send>> {
+        out.put_i64(self.microseconds);
+        out.put_i32(self.days);
+        out.put_i32(self.months);
 
         Ok(IsNull::No)
     }
 
     fn accepts(ty: &Type) -> bool {
-        match *ty {
-            INTERVAL => true,
+        match ty {
+            &Type::INTERVAL => true,
             _ => false,
         }
     }
 
-    postgres::to_sql_checked!();
+    postgres_types::to_sql_checked!();
 }
