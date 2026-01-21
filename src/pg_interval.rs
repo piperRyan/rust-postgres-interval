6ea2eba3f28164c45c26eb478ec879286753a718
Ning Sun <sunning@greptime.com>
2026-01-21 17:15:19 +0800

    style: format

--- src/pg_interval.rs
+++ src/pg_interval.rs
@@ -313,7 +313,7 @@ mod tests {
     }
 
     #[test]
-    fn test_postgres_19(){
+    fn test_postgres_19() {
         let interval = Interval::new(0, 3, 0);
         let output = interval.to_postgres();
         assert_eq!(String::from("3 days"), output);
@@ -444,5 +444,4 @@ mod tests {
         let output = interval.to_sql();
         assert_eq!(String::from("-1:10:15"), output);
     }
-
 }
